20f096fbbf145b65f89620abb3c0bf2e73ced1b9
StioStudio <simon.s.johanssen@gmail.com>
2022-07-21 23:12:23 +0200

    update

--- Functions.js
+++ Functions.js
@@ -352,7 +352,7 @@ function round(_num, amount=1){
     }
  * */
 function distance(x1, y1, x2, y2){
-    return( Math.sqrt((x1 - x2)**2 + (y1 - y2)**2) )
+    return( sqrt((x1 - x2)**2 + (y1 - y2)**2) )
 }
 
 /** @default
